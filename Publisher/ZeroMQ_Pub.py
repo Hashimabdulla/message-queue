b1dca7c35572700c0be24dd84633db2ec8119274
NithinGopakumar <nithin.da3@iiitmk.ac.in>
2020-02-20 11:17:59 +0530

    modified zeromq_pub

--- Publisher/ZeroMQ_Pub.py
+++ Publisher/ZeroMQ_Pub.py
@@ -6,12 +6,10 @@ sock = ctx.socket(zmq.PUB)
 sock.bind("tcp://127.0.0.1:1234")
 
 print("Starting loop...")
-i = 1
 while True:
-    msg = "Hello world %d:" % i
+    msg = "Hello world"
     sock.send_string(msg)
     print("Sent string: %s ..." % msg)
-    # i += 1
     time.sleep(1)
 # msg = "Hello world "
 # sock.send_string(msg)
