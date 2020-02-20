ce5dd68bdcad7c60414382669e801eca39b03b48
Hashimabdulla <hashimabdulla69@gmail.com>
2020-02-20 11:12:17 +0530

    add Zero_sub/pub files

--- Publisher/ZeroMQ_Pub.py
+++ Publisher/ZeroMQ_Pub.py
@@ -1,5 +1,20 @@
 import zmq
-context = zmq.Context()
-publisher = context.socket(zmq.PUB)
-publisher.bind("tcp://127.0.0.1:1234")
-publisher.send_multipart(["hello"])
+import time
+
+ctx = zmq.Context()
+sock = ctx.socket(zmq.PUB)
+sock.bind("tcp://127.0.0.1:1234")
+
+print("Starting loop...")
+i = 1
+while True:
+    msg = "Hello world %d:" % i
+    sock.send_string(msg)
+    print("Sent string: %s ..." % msg)
+    # i += 1
+    time.sleep(1)
+# msg = "Hello world "
+# sock.send_string(msg)
+# print("Sent string: %s ..." % msg)
+sock.close()
+ctx.term()
--- Subscriber/zeroMQ_Sub.py
+++ Subscriber/zeroMQ_Sub.py
@@ -1,6 +1,15 @@
 import zmq
-context=zmq.Context()
-subscriber = context.socket(zmq.SUB)
-subscriber.setsockopt_string(zmq.SUBSCRIBE, '')
-subscriber.connect('tcp://127.0.0.1:1234')
-subscriber.recv_multipart()
+import time
+
+ctx = zmq.Context()
+sock = ctx.socket(zmq.SUB)
+sock.connect("tcp://127.0.0.1:1234")
+sock.subscribe("") # Subscribe to all topics
+
+print("Starting receiver loop ...")
+while True:
+    msg = sock.recv_string()
+    print("Received string: %s ..." % msg)
+
+sock.close()
+ctx.term()
